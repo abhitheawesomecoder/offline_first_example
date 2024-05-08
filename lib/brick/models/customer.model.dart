ca902e0449e24655ea1d54d5b13db9446f7d23ab
abhishek.todquest <abhishek.todquest@gmail.com>
2024-05-08 11:42:03 +0530

    example not working

---
+++ lib/brick/models/customer.model.dart
@@ -0,0 +1,32 @@
+import 'package:brick_offline_first/brick_offline_first.dart';
+import 'package:brick_offline_first_with_rest/brick_offline_first_with_rest.dart';
+import 'package:brick_rest/brick_rest.dart';
+import 'package:brick_sqlite/brick_sqlite.dart';
+import 'package:getx_ex/brick/models/pizza.model.dart';
+import 'package:getx_ex/brick/models/customer.model.request.dart';
+import 'package:getx_ex/brick/models/pizza.model.dart';
+
+@ConnectOfflineFirstWithRest(
+  restConfig: RestSerializable(
+    requestTransformer: CustomerRequestTransformer.new,
+  ),
+)
+class Customer extends OfflineFirstWithRestModel {
+  @Sqlite(unique: true)
+  final int? id;
+
+  final String? firstName;
+
+  final String? lastName;
+
+  @OfflineFirst(where: {'id': "data['pizza_ids']"})
+  @Rest(name: 'pizza_ids')
+  final List<Pizza>? pizzas;
+
+  Customer({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.pizzas,
+  });
+}
